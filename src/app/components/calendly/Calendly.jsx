73ee305f56558dad2a60695825f4cd5925cb4161
devdharunprasad <devdharunprasad2002@gmail.com>
2023-06-06 16:21:57 +0530

    Edited calendly

--- src/app/components/calendly/Calendly.jsx
+++ src/app/components/calendly/Calendly.jsx
@@ -1,29 +1,33 @@
-"use client"
+"use client";
 import { useRouter } from "next/navigation";
 import React from "react";
-import {useCalendlyEventListener, InlineWidget } from "react-calendly";
+import { useCalendlyEventListener, InlineWidget } from "react-calendly";
 
 const Calendly = (props) => {
-  const router = useRouter()
-  
-  useCalendlyEventListener({
-   
-    onEventScheduled: (e) => {
+  const router = useRouter();
 
-      console.log(props.formData)
-      console.log(e.data.payload)
+  useCalendlyEventListener({
+    onEventScheduled: (e) => {
+      console.log(props.formData);
+      console.log(e.data);
       // if(e.data.payload) router.push("/success")
     },
   });
   return (
     <>
-    <div className="backdrop fixed top-0 left-0 w-full h-screen bg-black/40" onClick={() => props.setShowModal(false)}></div>
-    <div className="App fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white w-full">
-    <InlineWidget url="https://calendly.com/cape-connect/brainstorming-with-the-client"  prefill = {{
-      email : props.formData.email
-    }}/>
-  </div>
-  </>
+      <div
+        className="backdrop fixed top-0 left-0 w-full h-screen bg-black/40"
+        onClick={() => props.setShowModal(false)}
+      ></div>
+      <div className="App fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white w-full">
+        <InlineWidget
+          url="https://calendly.com/cape-connect/discovery-call?background_color=cc0000&text_color=ffffff&primary_color=000000"
+          prefill={{
+            email: props.formData.email,
+          }}
+        />
+      </div>
+    </>
   );
 };
 
